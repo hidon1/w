c3b77b9414216375a8d0ea43c64f7b5c881c229b
zeev moyal <ZM0548407450@gmail.com>
2026-02-14 20:32:00 +0200

    Update Firebase configuration for new project
    Updated Firebase configuration for the project, changing the API key and other settings.

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,23 @@
-// Firebase Configuration for hidon1 Project
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyABKn0GfHYi_1UG_0sfSn68CNNz4Q9nS7g",
-  authDomain: "hidon1-e4c91.firebaseapp.com",
-  databaseURL: "https://hidon1-e4c91-default-rtdb.firebaseio.com",
-  projectId: "hidon1-e4c91",
-  storageBucket: "hidon1-e4c91.firebasestorage.app",
-  messagingSenderId: "411517496015",
-  appId: "1:411517496015:web:2d9c176783d062110465ba",
-  measurementId: "G-FWTSZNY72T"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Export to window for use in index.html
-if (typeof window !== 'undefined') {
-  window.firebaseConfig = firebaseConfig;
-}
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_tulsSjZTB0F0ya34N2XUD67a77BTWpw",
+    authDomain: "wine-f57cb.firebaseapp.com",
+    projectId: "wine-f57cb",
+    storageBucket: "wine-f57cb.firebasestorage.app",
+    messagingSenderId: "172638778743",
+    appId: "1:172638778743:web:85045c1b2eeefeb2c11d72",
+    measurementId: "G-6D6L2W94D6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
