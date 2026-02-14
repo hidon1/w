a2d00d98725ec966951b587d175899c9b7db0358
zeev moyal <ZM0548407450@gmail.com>
2026-02-14 19:17:17 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,11 @@
-window.firebaseConfig = {
-    apiKey: 'AIzaSyABKn0GfHYi_1UG_0sfSn68CNNz4Q9nS7g',
-    authDomain: 'hidon1-e4c91.firebaseapp.com',
-    projectId: 'hidon1-e4c91',
-    storageBucket: 'hidon1-e4c91.firebasestorage.app',
-    messagingSenderId: '411517496015',
-    appId: '1:411517496015:web:2d9c176783d062110465ba'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABKn0GfHYi_1UG_0sfSn68CNNz4Q9nS7g",
+  authDomain: "hidon1-e4c91.firebaseapp.com",
+  databaseURL: "https://hidon1-e4c91-default-rtdb.firebaseio.com",
+  projectId: "hidon1-e4c91",
+  storageBucket: "hidon1-e4c91.firebasestorage.app",
+  messagingSenderId: "411517496015",
+  appId: "1:411517496015:web:2d9c176783d062110465ba",
+  measurementId: "G-FWTSZNY72T"
 };
