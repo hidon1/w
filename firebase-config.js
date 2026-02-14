85b31f2c9192972aff7a4f074dd8fda73b9efdf5
zeev moyal <ZM0548407450@gmail.com>
2026-02-14 19:07:29 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.firebaseConfig = {
-    apiKey: 'REPLACE_WITH_FIREBASE_API_KEY',
-    authDomain: 'REPLACE_WITH_FIREBASE_AUTH_DOMAIN',
-    projectId: 'REPLACE_WITH_FIREBASE_PROJECT_ID',
-    storageBucket: 'REPLACE_WITH_FIREBASE_STORAGE_BUCKET',
-    messagingSenderId: 'REPLACE_WITH_FIREBASE_SENDER_ID',
-    appId: 'REPLACE_WITH_FIREBASE_APP_ID'
+    apiKey: 'AIzaSyABKn0GfHYi_1UG_0sfSn68CNNz4Q9nS7g',
+    authDomain: 'hidon1-e4c91.firebaseapp.com',
+    projectId: 'hidon1-e4c91',
+    storageBucket: 'hidon1-e4c91.firebasestorage.app',
+    messagingSenderId: '411517496015',
+    appId: '1:411517496015:web:2d9c176783d062110465ba'
 };
